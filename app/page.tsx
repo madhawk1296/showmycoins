e918d80ca40ae8087581e41e967e45675f79b059
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2023-07-03 21:50:18 -0600

    added api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-export default function Home() {
+import fetchCoins from "./lib/fetchCoins"
+
+export default async function Home() {
+  const coins = await fetchCoins()
+
   return (
     <main className="">
+
     </main>
   )
 }
