6f8967ab5a465e62a43eaae7fe705e641bbfadc3
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2023-07-08 21:06:26 -0600

    Bugs

--- lib/formatPercent.ts
+++ lib/formatPercent.ts
@@ -1,5 +1,5 @@
 import numeral from 'numeral'
 
-export default function formatPercent(numValue: number): numeral{
+export default function formatPercent(numValue: number){
     return numeral(numValue).format('%0,0.00')
 }
--- lib/formatToken.ts
+++ lib/formatToken.ts
@@ -1,5 +1,5 @@
 import numeral from 'numeral'
 
-export default function formatToken(numValue: number): numeral{
+export default function formatToken(numValue: number){
     return numeral(numValue).format('0,0.[0000]')
 }
