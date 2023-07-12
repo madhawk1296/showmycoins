e7a24fc370007426e19178c9dfd8ec217d7f959d
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2023-07-11 18:41:41 -0600

    Added condition to api calls

--- lib/fetchChainCoins.ts
+++ lib/fetchChainCoins.ts
@@ -1,12 +1,17 @@
-export default async function fetchChainCoins(userId: string, chainId: string): Promise<JSON>{
+export default async function fetchChainCoins(userId: string, chainId: string, repeat=false) {
     const res = await fetch(`https://api.n.xyz/api/v1/address/${userId}/balances/fungibles?chainID=${chainId}&filterDust=false&filterSpam=false&apikey=${process.env.NXYZ_API}`, {next: { revalidate: 10 }})
 
     if (res.status === 429) {
-        // Wait for 1 second
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
-        // Retry the request
-        return fetchChainCoins(userId, chainId);
+        if(repeat) {
+            // Wait for 1 second
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            
+            
+            // Retry the request
+            return fetchChainCoins(userId, chainId, false);
+            } else {
+            return []
+            }
     }
 
     return res.json()
