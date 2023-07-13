689c533170baed75f82a644ec16a5cd4ed9b0706
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2023-07-12 20:52:51 -0600

    Updated Desktop Tables

--- app/[userId]/Coin.tsx
+++ app/[userId]/Coin.tsx
@@ -16,12 +16,21 @@ export default function Coin({coin, total}: {coin: any, total:number}){
 
     return (
         <div className="relative w-full flex-wrap md:h-[75px] flex items-center justify-between px-[20px] bg-white">
-            <CoinDetail detail={name} image={uri} containsImage={true}/>
-            <CoinDetail detail={chain} />
-            <CoinDetail detail={`${symbol}`} />
-            <CoinDetail detail={formatToken(tokenValue)} />
-            <CoinDetail detail={formatDollar(dollarValue)} />
-            <CoinDetail detail={formatPercent(percentage)} />
+            <div className="relative flex items-center gap-2">
+                <CoinDetail detail={name} image={uri} containsImage={true}/>
+                <h1 className="text-gray-500 text-sm font-bold"> ({chain})</h1>
+            </div>
+            <div className="relative flex flex-row md:w-[500px] flex items-center justify-between">
+                <div className="w-[80px]">
+                    <CoinDetail detail={formatToken(tokenValue)} /> 
+                </div>
+                <div className="w-[80px]">
+                    <CoinDetail detail={formatDollar(dollarValue)} isGreen={true} />
+                </div>
+                <div className="w-[80px]">
+                    <CoinDetail detail={formatPercent(percentage)} />
+                </div>
+             </div>
         </div>
     )
 }
--- app/[userId]/TitleName.tsx
+++ app/[userId]/TitleName.tsx
@@ -1,5 +1,5 @@
 export default function TitleName({name}: {name: string}){
     return (
-        <h1 className="text-center md:w-[180px] text-md font-medium text-gray-500">{name}</h1>
+        <h1 className="text-center md:w-full text-md font-medium text-gray-700 whitespace-nowrap">{name}</h1>
     )
 }
