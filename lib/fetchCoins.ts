5fe2e3fdff78a623f8fa5c74ba586d59334a8052
madhawk1296 <silvacristian1296@gmail.com>
2023-09-13 19:40:05 -0600

    fix bug

--- lib/fetchCoins.ts
+++ lib/fetchCoins.ts
@@ -5,9 +5,11 @@ import fetchTokens from "./fetchTokens";
 export default async function fetchCoins(userId: string, chain: string): Promise<Coin[]> {
     const coins = await fetchTokens(userId, chain);
     const eth = await fetchNativeToken(userId, chain);
-    
+
+    console.log([...coins, eth])
+
     return [...coins, eth]
-    .filter((coin: any) => coin.possible_spam == false && !coin.token_address)
+    .filter((coin: any) => coin.possible_spam == false && coin.address)
     .map((coin: any) => {
         return {address: coin.token_address ,chain, ...coin}
     }); 
--- lib/fetchTokens.ts
+++ lib/fetchTokens.ts
@@ -1,11 +1,15 @@
-import { EvmChain } from "@moralisweb3/common-evm-utils";
+import { Coin } from "@/types/Coin";
 
-export default async function fetchTokens(userId: string, chain: string) {
+export default async function fetchTokens(userId: string, chain: string): Promise<Coin[]> {
     const res = await fetch(`https://deep-index.moralis.io/api/v2.2/${userId}/erc20?chain=${chain}`, {
         headers: {
             'X-API-Key': process.env.MORALIS_API_KEY!
         }
     })
 
-    return await res.json();
+    const data = await res.json();
+
+    return data.map((item: any) => {
+        return {...item, address: item.token_address }
+    })
 }
